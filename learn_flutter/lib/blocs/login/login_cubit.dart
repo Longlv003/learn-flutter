b067f47e1652a0c0096738736e250a2fee3b116d
Longlv003 <longlv003@gmail.com>
2026-04-08 09:35:36 +0700

    feat(2026-04-08): Viết giao diện đăng nhập
    - Viết LoginCubit xử lý đăng nhập.

--- learn_flutter/lib/blocs/login/login_cubit.dart
+++ learn_flutter/lib/blocs/login/login_cubit.dart
@@ -1,1 +1,34 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:learn_flutter/blocs/login/login_state.dart';
 
+class LoginCubit extends Cubit<LoginState> {
+  LoginCubit() : super(LoginInitial());
+
+  Future<void> loginHandle(String email, String pass) async {
+    try {
+      emit(LoginLoading());
+
+      await Future.delayed(const Duration(seconds: 1));
+
+      if (email.trim().isEmpty || pass.trim().isEmpty) {
+        emit(LoginFailed("Email và pass không được để trống!"));
+        return;
+      }
+
+      if (!email.trim().endsWith("@gmail.com")) {
+        emit(LoginFailed("Email sai định dạng!"));
+        return;
+      }
+
+      await Future.delayed(const Duration(seconds: 1));
+
+      if (email.trim() == "admin@gmail.com" && pass.trim() == "admin123") {
+        emit(LoginSuccess());
+      } else {
+        emit(LoginFailed("Sai tài khoản hoặc mật khẩu!"));
+      }
+    } catch (e) {
+      emit(LoginFailed(e.toString()));
+    }
+  }
+}
--- learn_flutter/lib/pages/login_page.dart
+++ learn_flutter/lib/pages/login_page.dart
@@ -1,10 +1,174 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
+import 'package:learn_flutter/blocs/login/login_cubit.dart';
+import 'package:learn_flutter/blocs/login/login_state.dart';
+import 'package:learn_flutter/routes/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _obscureText = true;
+
+  final _emailController = TextEditingController();
+  final _passController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Login UI")));
+    return BlocProvider(
+      create: (_) => LoginCubit(),
+      child: BlocListener<LoginCubit, LoginState>(
+        listener: (context, state) {
+          if (state is LoginSuccess) {
+            Get.offAllNamed(Routes.homePage);
+          }
+        },
+        child: Scaffold(body: Stack(children: [_buildBody(), _buildLoading()])),
+      ),
+    );
+  }
+
+  Widget _buildBody() {
+    return Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: BlocBuilder<LoginCubit, LoginState>(
+            builder: (context, state) {
+              final loginCubit = context.read<LoginCubit>();
+              final isLoading = state is LoginLoading;
+              final errorMessage = state is LoginFailed ? state.error : null;
+
+              return Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    "Chào mừng bạn",
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  const Text(
+                    "Đăng nhập tài khoản",
+                    style: TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
+                  const SizedBox(height: 30),
+
+                  /// Email
+                  TextFormField(
+                    controller: _emailController,
+                    cursorColor: const Color(0xFF1B8E42),
+                    decoration: InputDecoration(
+                      labelText: "Email",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                          color: Color(0xFF1B8E42),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 15),
+
+                  /// Password
+                  TextFormField(
+                    controller: _passController,
+                    obscureText: _obscureText,
+                    cursorColor: const Color(0xFF1B8E42),
+                    decoration: InputDecoration(
+                      labelText: "Password",
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: const BorderSide(
+                          color: Color(0xFF1B8E42),
+                          width: 2,
+                        ),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureText
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () =>
+                            setState(() => _obscureText = !_obscureText),
+                      ),
+                    ),
+                  ),
+
+                  /// Error
+                  if (errorMessage != null)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          errorMessage,
+                          style: const TextStyle(
+                            color: Colors.red,
+                            fontSize: 12,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                  const SizedBox(height: 20),
+
+                  /// Button
+                  SizedBox(
+                    width: double.infinity,
+                    height: 55,
+                    child: ElevatedButton(
+                      onPressed: isLoading
+                          ? null
+                          : () {
+                              loginCubit.loginHandle(
+                                _emailController.text,
+                                _passController.text,
+                              );
+                            },
+                      child: const Text("Login"),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoading() {
+    return BlocBuilder<LoginCubit, LoginState>(
+      builder: (context, state) {
+        if (state is! LoginLoading) return const SizedBox();
+
+        return Container(
+          color: Colors.black.withAlpha(76),
+          child: const Center(child: CircularProgressIndicator()),
+        );
+      },
+    );
   }
 }
